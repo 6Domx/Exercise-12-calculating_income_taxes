f93366ac8fd6fc21c8bb7e9b3cc61b8ff903c283
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:49:52 +0800

    Code finalized and labeled

--- Exercise-12-calculating_income_taxes.py
+++ Exercise-12-calculating_income_taxes.py
@@ -5,6 +5,7 @@
 # Next $10,000 = 10 rate in %
 # Proceeding = 20 rate in %
 
+# For asking user income
 user_income = input("Please input your income: ")
 if user_income.isdigit():
     user_income = int(user_income)
@@ -14,16 +15,19 @@ else:
 if user_income <= 10000:
     tax_rate = 0
 
+# 0 rate in first 10,000
 elif user_income <= 20000:
     first_ten_thou = user_income - 10000
 
     tax_rate = first_ten_thou * 10 / 100
 
 else: 
+# first rate 
     tax_rate = 0
-
+# second rate
     tax_rate = 10000 * 10 / 100
-
+# third rate
     tax_rate += (user_income - 20000) * 20 / 100
 
+# For printing tax payable
 print("Your need to pay $", tax_rate)
