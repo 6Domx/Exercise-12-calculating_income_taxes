c7b2277122134e5616a53a2e55a15467061923bb
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:38:59 +0800

    Pseudocode

---
+++ Exercise-12-calculating_income_taxes.py
@@ -0,0 +1,6 @@
+# Pseudocode
+# 1. Create a program that will calculate income taxes
+# 2. Follow the rules:
+# First $10,000 = 0 rate in %
+# Next $10,000 = 10 rate in %
+# Proceeding = 20 rate in %
