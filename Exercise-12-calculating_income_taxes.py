b97d3467ef4eb2d86493340d7cc2102e3602877c
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:44:22 +0800

    first rate is now added

--- Exercise-12-calculating_income_taxes.py
+++ Exercise-12-calculating_income_taxes.py
@@ -10,3 +10,10 @@ if user_income.isdigit():
     user_income = int(user_income)
 else:
     print("Invalid, please input numbers.")
+
+if user_income <= 10000:
+    tax_rate = 0
+elif user_income <= 20000:
+    first_ten_thou = user_income - 10000
+
+    tax_rate = first_ten_thou * 10 / 100
